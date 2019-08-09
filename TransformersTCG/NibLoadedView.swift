b2cf76009fa51fd6a66ff6d016b82d791aa9823e
Andrei Freeman <andrei@lordandrei.com>
2019-08-09 14:04:24 -0400

    Renames Xib files to match swift definition files. Adds NibLoadedView

---
+++ TransformersTCG/NibLoadedView.swift
@@ -0,0 +1,48 @@
+//
+//  NibLoadedView.swift
+//  TransformersTCG
+//
+//  Created by Andrei Freeman on 8/9/19.
+//  Copyright © 2019 LordAndrei dot com. All rights reserved.
+//
+import UIKit
+
+class NibLoadedView: UIView {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    init() {
+        super.init(frame: .zero)
+        
+        self.initialize()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.initialize()
+    }
+    
+    func initialize() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        let name = String(describing: type(of: self))
+        let nib = UINib(nibName: name, bundle: .main)
+        nib.instantiate(withOwner: self, options: nil)
+        
+        self.addSubview(self.containerView)
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.containerView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            ])
+    }
+}
