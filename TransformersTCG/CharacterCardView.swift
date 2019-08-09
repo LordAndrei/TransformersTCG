0fbfef59d460b96cadf351171d0a2e471af14af4
Aiden Freeman <Aiden@freeple.org>
2019-08-09 13:18:52 -0400

    Begun source code for CharacterCards, and added fourth class to design.

---
+++ TransformersTCG/CharacterCardView.swift
@@ -0,0 +1,28 @@
+//
+//  CharacterCardView.swift
+//  TransformersTCG
+//
+//  Created by Aiden Freeman on 8/9/19.
+//  Copyright © 2019 LordAndrei dot com. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCardView: UIView {
+
+    @IBOutlet var name: UILabel!
+    @IBOutlet var discription: UILabel!
+    @IBOutlet var mode: UILabel!
+    @IBOutlet var otherMode: UILabel!
+    @IBOutlet var attackPower: UILabel!
+    @IBOutlet var health: UILabel!
+    @IBOutlet var defencePower: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
