8a0b343ac517942e862f50b87a5a572a90e13795
Andrei Freeman <andrei@lordandrei.com>
2019-08-13 10:49:59 -0400

    Modifies flow for controllers

---
+++ TransformersTCG/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  TransformersTCG
+//
+//  Created by Andrei Freeman on 8/10/19.
+//  Copyright © 2019 LordAndrei dot com. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  @IBAction func returnFromModal(_ segue: UIStoryboardSegue) {
+    
+  }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
