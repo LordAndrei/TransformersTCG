d129e8e5ee3ffd0ca7c1ffa72d9c5121cd3e8919
Andrei Freeman <andrei@lordandrei.com>
2019-08-31 16:50:23 -0400

    Fix Set to Array for character cards Rename players to gamePlayers Create playerCharacterCards Add to players.

--- TransformersTCG/GamePlay.swift
+++ TransformersTCG/GamePlay.swift
@@ -29,14 +29,16 @@ extension PlayerCharacterCard: Hashable {
 }
 
 class Player {
-    var characterCards:Set<PlayerCharacterCard> = []
+    var characterCards:Array<PlayerCharacterCard> = []
 }
 
 class GamePlay {
-    var players:[Player] = []
+    var gamePlayers:[Player] = []
     
     func dealCharacterCards() {
         for playerIndex in 1...2 {
+            // Make a player (has bucket of player character cards)
+            let aPlayer = Player()
             print("Dealing Cards for Player: \(playerIndex)")
             var setOfCharacterCards: Set<CharacterCard> = []
             while setOfCharacterCards.count < 2 {
@@ -47,11 +49,19 @@ class GamePlay {
             print("Cards: ")
             var starCountTotal = 0
             for card in setOfCharacterCards {
-                print("Name: \(card.name)")
+                // make a player characterCard
+                let aPlayerCharacterCard = PlayerCharacterCard()
+                // add it to the player's bucket of player playing Cards
+              aPlayer.characterCards.append(aPlayerCharacterCard)
+                
+              print("Name: \(card.name)")
                 starCountTotal += card.starCount
             }
             print("Total Star Count: \(starCountTotal)")
+            // Add the player to the players
+            gamePlayers.append(aPlayer)
         }
+        print("GamePlayers: \(gamePlayers)")
     }
     
     func dealBattleCards() {
